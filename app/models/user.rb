00e26ec2ab0e809c016a1e1c6e4f43fc0d183323
sean <midruss@yahoo.com>
2022-02-26 10:41:48 -0500

    Created users table, user model, wrote all backend auth code in user model and application_controller.rb

---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+    validates :first_name, :last_name, :username, :password_digest, :session_token presence: true
+    validates :username, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_validation :ensure_session_token
+    
+    attr_reader :password
+
+    # add associations after other db tables are generated
+    # has_many :reservations, :reviews, :saves
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
+end
