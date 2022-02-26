bccc7aeaadb8cf74f0e546bf58ae149796083188
sean <midruss@yahoo.com>
2022-02-26 13:02:39 -0500

    Wrote store.js, intstalled redux-thunk, added getState to the window and confirmed that state shape matches my expectation of how it should currently look

--- frontend/empty_chairs.jsx
+++ frontend/empty_chairs.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {signup, login, logout} from "./util/session_api_util"
+import configureStore from "./store/store"
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    const store = configureStore();
     ReactDOM.render(<h1>Welcome to EmptyChairs!</h1>, root);
 
     // test
     window.signup = signup;
     window.login = login;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;      
     // test
 });
