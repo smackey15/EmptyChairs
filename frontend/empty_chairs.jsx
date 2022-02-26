24e3ed4c71773ab473935c82681eff1dea5f8e2d
sean <midruss@yahoo.com>
2022-02-26 12:18:49 -0500

    Wrote session_api.util.js and successfully tested those ajax methods (signup, login, logout) on the window

--- frontend/empty_chairs.jsx
+++ frontend/empty_chairs.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {signup, login, logout} from "./util/session_api_util"
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to EmptyChairs!</h1>, root);
+
+    // test
+    window.signup = signup;
+    window.login = login;
+    window.logout = logout;
+    // test
 });
