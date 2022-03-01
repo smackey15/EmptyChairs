974688b34cae69b6ab26ed1ef2aee51ee3d61993
sean <midruss@yahoo.com>
2022-03-01 07:39:41 -0500

    Updated login_form and signup_form containers to work with modal

--- frontend/components/session/login_form_container.jsx
+++ frontend/components/session/login_form_container.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {login} from "../../actions/session_actions"
 import { login } from "../../actions/session_actions";
 import SessionForm from "./session_form"
-import {Link} from "react-router-dom";
+import { openModal, closeModal } from "../../actions/modal_actions";
+// import {Link} from "react-router-dom";
+// import {login} from "../../actions/session_actions"
+
 
 const mapStateToProps = (state, ownProps) => ({ 
     errors: state.errors.session,               
     formType: "Sign In to an account",
-    link: <Link to="/signup" className="session-switch-link">Sign Up</Link>
+    // link: <Link to="/signup" className="session-switch-link">Sign Up</Link>
 
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    processForm: (user) => dispatch(login(user))
+    processForm: (user) => dispatch(login(user)),
+    otherForm: (
+        <button onClick={() => dispatch(openModal("signup"))}>
+            Signup
+        </button>
+    ),
+    closeModal: () => dispatch(closeModal())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
--- frontend/components/session/signup_form_container.jsx
+++ frontend/components/session/signup_form_container.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {signup} from "../../actions/session_actions"
 import { signup } from "../../actions/session_actions";
 import SessionForm from "./session_form"
-import {Link} from "react-router-dom";
+import { openModal, closeModal } from "../../actions/modal_actions";
+// import {signup} from "../../actions/session_actions"
+// import {Link} from "react-router-dom";
 
 const mapStateToProps = (state, ownProps) => ({ //?
     errors: state.errors.session,               //?
     formType: "Create an account",
-    link: <Link to="/signin" className="session-switch-link">Sign In</Link>
+    // link: <Link to="/signin" className="session-switch-link">Sign In</Link>
 
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-    processForm: (user) => dispatch(signup(user))
+    processForm: (user) => dispatch(signup(user)),
+    otherForm: (
+        <button onClick={() => dispatch(openModal("login"))}>
+            Signin
+        </button>
+    ),
+    closeModal: () => dispatch(closeModal())
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
