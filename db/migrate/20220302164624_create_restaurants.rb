6ad224455d84cc0c288cf4db46e46bb22b5ab7b2
sean <midruss@yahoo.com>
2022-03-02 12:14:02 -0500

    started restuarant migration table

--- db/migrate/20220302164624_create_restaurants.rb
+++ db/migrate/20220302164624_create_restaurants.rb
@@ -1,6 +1,8 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
     create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
