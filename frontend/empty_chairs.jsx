6058acc0f9cdcb5d4f51366a762516998c306fa0
sean <midruss@yahoo.com>
2022-03-02 16:51:35 -0500

    Coded restaurant_actions.js and successfully dispatch of actions on the window

--- frontend/empty_chairs.jsx
+++ frontend/empty_chairs.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import {signup, login, logout} from "./util/session_api_util"
 // import { signup, login, logout } from "./actions/session_actions";
-import { fetchRestaurant, fetchRestaurants } from "./util/restaurant_api_util";
+// import { fetchRestaurant, fetchRestaurants } from "./util/restaurant_api_util";
+import { fetchRestaurant, fetchRestaurants } from "./actions/restaurant_actions";
 import configureStore from "./store/store"
 import Root from "./components/root"
 import { FaWindowRestore } from "react-icons/fa";
