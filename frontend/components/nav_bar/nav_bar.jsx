b77daa2c307c5dec25b391b6e157dd985c0f1910
sean <midruss@yahoo.com>
2022-04-01 15:49:36 -0400

    add dropdown and signout functionality to user icon

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,23 +4,46 @@ import {HiOutlineUser} from "react-icons/hi"
 import {AiOutlineCalendar} from "react-icons/ai"
 import {FaRegBell} from "react-icons/fa"
 
-const NavBar = ({ currentUser, logout, openModal }) => {            
-    const display = currentUser ? (
-        <div className="icons">
-            <p className="greeting">Hello, {currentUser.first_name}!</p>
-            <HiOutlineUser />
-            <AiOutlineCalendar />
-            <FaRegBell />
-            <button onClick={logout} className="sign-out-button">Sign out</button>
-            {/* above icons needs to be dropdowns with hover message */}
-        </div>
-    ) : (
-        <div>
-            <button className="signup-button" onClick={() => openModal("signup")}>Sign up</button>
-            <button className="signin-button" onClick={() => openModal("login")}>Sign in</button>
-        </div>
-    )
+// const NavBar = ({ currentUser, logout, openModal }) => {            
+//     const display = currentUser ? (
+//         <div className="icons">
+//             <p className="greeting">Hello, {currentUser.first_name}!</p>
+            
+//             <a href="#"><HiOutlineUser /></a>
+//             <ul>
+//                 <li><button onClick={logout} className="sign-out-button">Sign out</button></li>
+//             </ul>
+//             {/* <AiOutlineCalendar />
+//             <FaRegBell /> */}
+//             {/* <button onClick={logout} className="sign-out-button">Sign out</button> */}
+//             {/* above icons needs to be dropdowns with hover message */}
+//         </div>
+//     ) : (
+//         <div>
+//             <button className="signup-button" onClick={() => openModal("signup")}>Sign up</button>
+//             <button className="signin-button" onClick={() => openModal("login")}>Sign in</button>
+//         </div>
+//     )
 
+    const NavBar = ({ currentUser, logout, openModal }) => {            
+        const display = currentUser ? (
+            <div className="icons">
+                <p className="greeting">Hello, {currentUser.first_name}!</p>
+                    
+                    <div className="dropdown">
+                            <p className="dropbtn"><HiOutlineUser /></p>
+                            <ul className="dropdown-content">
+                                <li onClick={logout}>Sign out</li>
+                            </ul>
+                    </div>
+            </div>
+        ) : (
+            <div>
+                <button className="signup-button" onClick={() => openModal("signup")}>Sign up</button>
+                <button className="signin-button" onClick={() => openModal("login")}>Sign in</button>
+            </div>
+        )
+    
 return (
     <div>{display}</div>
     )
