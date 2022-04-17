e7229d81ce57dcec8a7e57e29af52d977ab7337a
sean <midruss@yahoo.com>
2022-04-17 18:44:15 -0400

    start styling reservation index items

--- frontend/components/reservations/reservation_index_item.jsx
+++ frontend/components/reservations/reservation_index_item.jsx
@@ -13,9 +13,9 @@ import { Link } from 'react-router-dom';
 
 const ReservationIndexItem = ({ reservation }) => {
     return(
-        <div className='respond'>
-                <li className='index-body'>
-                    <img className='photo-main-index' src={reservation.photoUrl} alt="" />
+        <div className='res-respond'>
+                <li className='res-index-body'>
+                    <img className='res-photo-main-index' src={reservation.photoUrl} alt="" />
                     <span className='item-name'>{reservation.restaurantName}</span>
                     <span className='item-address'>{reservation.party_size}</span>
                     <span className='item-address'>{reservation.date}</span>
