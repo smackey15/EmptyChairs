56ae88e8af23995b86563756c15876d75ebc92ca
sean <midruss@yahoo.com>
2022-04-18 11:25:34 -0400

    style reservation index item tiles

--- frontend/components/reservations/reservation_index_item.jsx
+++ frontend/components/reservations/reservation_index_item.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// function randomnum (min, max) {
-//     min = Math.ceil(min)
-//     max = Math.floor(max)
-//     return Math.floor(Math.random() * (max-min) + min);
-// }
-// function randomstar (min, max) {
-//     return Math.floor(Math.random() * (max-min) + min);
-// }
-
+import {HiOutlineUser} from "react-icons/hi"
+import {AiOutlineCalendar} from "react-icons/ai"
 
 const ReservationIndexItem = ({ reservation }) => {
     return(
         <div className='res-respond'>
                 <li className='res-index-body'>
                     <img className='res-photo-main-index' src={reservation.photoUrl} alt="" />
-                    <span className='item-name'>{reservation.restaurantName}</span>
-                    <span className='item-address'>{reservation.party_size}</span>
-                    <span className='item-address'>{reservation.date}</span>
-                    <Link to={`/restaurants/${reservation.restaurant_id}/reviews/form`}><p className='reserve-button'>Leave a review</p></Link>
+                    <div className='res-content'>
+                        <span className='res-item-name'>{reservation.restaurantName}</span>
+                        <div className='res-details'>
+                            <span className='item-address'>{reservation.party_size}</span>
+                            <span className='item-address'>{reservation.date}</span>
+                        </div>
+                        <Link to={`/restaurants/${reservation.restaurant_id}/reviews/form`} className='review-link'><p className='review-link'>Leave a review</p></Link>
+                    </div>
                 </li>
 
         </div>
