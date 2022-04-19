a2e77392f3b8104c62dabb3dbf3ba3c0c90b2be6
sean <midruss@yahoo.com>
2022-04-19 14:35:47 -0400

    finish styling user profile page

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -4,18 +4,17 @@ import ReservationIndexContainer from "../reservations/reservation_index_contain
 
 const UserShow = ({ currentUser }) => {
 
-    return 
+    return (
         <div>
             <div className="user-nav">
                 <h1 className="user-name">{currentUser.first_name} {currentUser.last_name}</h1>
                 <Link to={`/userprofile`} className="res-link"><h3>Reservations</h3></Link>
-                <h3>Saved Restaurants</h3>
+                <Link to={`/myfavorites`} className="sav-link"><h3>Saved Restaurants</h3></Link>
             </div>
 
             <ReservationIndexContainer
                 currentUser={currentUser}
             />
-
         </div>
     )
 }
