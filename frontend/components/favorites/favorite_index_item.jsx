209bb5ed07d89e91c9d028e2994622148e2e9095
sean <midruss@yahoo.com>
2022-04-20 10:49:21 -0400

    add star ratings to saved restaurants index item

--- frontend/components/favorites/favorite_index_item.jsx
+++ frontend/components/favorites/favorite_index_item.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { IoMdBookmark } from "react-icons/io"
+import { IoMdBookmark } from "react-icons/io";
+import StarRatings from 'react-star-ratings';
 
 const FavoriteIndexItem = ({ favorite, deleteFavorite, restaurant }) => {
     return(
 
         <div>
                 <li className='sav-index-body'>
-                    {restaurant ? <img className='sav-photo-main-index' src={restaurant.photoUrl} alt="" /> : ""}
+                    <img className='sav-photo-main-index' src={restaurant?.photoUrl} alt="" /> 
                     <div className="sav-content">
-                        {restaurant ? <span className='sav-item-name'>{restaurant.name}</span> : ""}
+                        <span className='sav-item-name'>{restaurant?.name}</span>
                         <div className='remove'>
                             <IoMdBookmark className='sav-icon' />
                             <span className="sav-delete" onClick={() => deleteFavorite(favorite.id)}>Remove from saved restaurants</span>
                         </div>
-                        <span className='sav-rate'>rating</span>
-                        <span className='sav-address'>cuisine/neighborhood</span>
+                        {/* <span className='sav-rate'>rating</span> */}
+                        <StarRatings 
+                            rating={3.75}
+                            starDimension="18px"
+                            starSpacing=".5px"
+                            starRatedColor='#feab6c'
+                        />
+                        <span className='sav-address'>{restaurant?.address}</span>
                     </div>
                     {restaurant ? <Link to={`/restaurants/${restaurant.id}`} className="sav-res-button-position"><button className="sav-reserve-button">Reserve now</button></Link> : ""}
                 </li>
